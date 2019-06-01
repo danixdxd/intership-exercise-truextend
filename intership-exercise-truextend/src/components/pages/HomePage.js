a13e2494bf7081a7a7aadebbc9bc96c9c9651e07
Daniel <dany.orop@gmail.com>
2019-06-01 16:17:50 -0400

    homePage Implemented

--- intership-exercise-truextend/src/components/pages/HomePage.js
+++ intership-exercise-truextend/src/components/pages/HomePage.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React from 'react'; 
+import { Button } from 'semantic-ui-react';
 
-const HomePage = () => {
-    return(<div>
-        HomePage
-    </div>);
+import "../../styles/templates/HomePage.css";
+
+class  HomePage  extends React.Component {
+    constructor(props) {
+        super(props);
+        this.goToRepositories = this.goToRepositories.bind(this);
+    }
+    goToRepositories() {
+        this.props.history.push('/repositories');
+    }
+    render() {
+        return (
+            <div className="homePage center-div">
+                <div className="black-background">
+                    <Button animated fluid inverted color="orange" size="massive" className="button-homepage" onClick={this.goToRepositories}>
+                        <Button.Content visible>Click Aqui</Button.Content>
+                        <Button.Content hidden>Repositorios</Button.Content>
+                    </Button>
+                </div>
+            </div>
+        )
+    }
 }
-export default HomePage;
+export  default  HomePage;
