4181a43d961bc3250b584aa035421addaa436958
Daniel <dany.orop@gmail.com>
2019-06-01 15:56:35 -0400

    semantic-ui-react, semantic-ui-react installation. Creation of pages

--- intership-exercise-truextend/src/App.js
+++ intership-exercise-truextend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
+import HomePage from "../src/components/pages/HomePage";
+import RepositoriesPage from "../src/components/pages/RepositoriesPage";
+import AboutPage from "../src/components/pages/AboutPage";
+import ErrorPage from "../src/components/pages/ErrorPage";
 
 function App() {
   return (
       <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/repositories" exact component={RepositoriesPage} />
+        <Route path="/about" exact component={AboutPage} />
+        <Route exact component={ErrorPage} />
       </Switch>
   );
 }
---
+++ intership-exercise-truextend/src/components/pages/RepositoriesPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const RepositoriesPage = () => {
+    return(<div>
+        RepositoriesPage
+    </div>);
+}
+export default RepositoriesPage;
